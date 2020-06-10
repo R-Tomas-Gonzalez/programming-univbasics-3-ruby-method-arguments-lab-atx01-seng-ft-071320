4b3e4224dcec7aa1c99b94dfcc24f028aa5b4756
Tomas Gonzalez <tomas@rtomasgonzalez.com>
2020-06-10 02:02:31 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -7,6 +7,12 @@ def introduction(name)
 end
 introduction('Tomas')
 
+def introduction_with_language(name, language)
+  puts "Hi, my name is #{name} and I am learning to program in #{language}."
+end
+introduction_with_language("Tomas", "Ruby")
+
+
 
 
 
